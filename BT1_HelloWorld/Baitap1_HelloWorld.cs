573c3fcd7e6408c0390c37a6f3490fe2c4a75fa5
DyHon <lehoang8d@gmail.com>
2021-09-12 21:59:42 +0700

    BT1 - Submitted

---
+++ BT1_HelloWorld/Baitap1_HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DuyHoang_IS220.M12_framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
